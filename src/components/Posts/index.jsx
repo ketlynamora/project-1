8b796a9fe66f3707d540451519c4fb11842440b3
Ketlyn Amora <ketypricila70@gmail.com>
2022-09-03 19:21:21 -0300

    props e organizacao dos componentes part 2

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,17 @@
+import { PostCard } from "../PostCard";
+
+export const Posts = ({ posts }) => (
+  <div className="posts">
+    {posts.map((post) => (
+      <PostCard
+        key={post.id}
+        post={post}
+        // OR
+        // id={post.id}
+        // title={post.title}
+        // body={post.body}
+        // cover={post.cover}
+      />
+    ))}
+  </div>
+);
