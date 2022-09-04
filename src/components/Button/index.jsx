3fada5db5bbc82e87676955ce7c63113d1d9551f
Ketlyn Amora <ketypricila70@gmail.com>
2022-09-04 19:10:05 -0300

    correção do button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,7 @@
 import "./styles.css";
 
-export const Button = ({ text, onClick, disabled }) => {
+export const Button = ({ text, onClick, disabled }) => (
   <button className="button" onClick={onclick} disabled={disabled}>
     {text}
-  </button>;
-};
+  </button>
+);
